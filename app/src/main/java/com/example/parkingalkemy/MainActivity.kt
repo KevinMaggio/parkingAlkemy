270dbbfe3afcb176264ded01e2630b339590e0cd
KevinMaggio <kevin.maggio@mercadolibre.com>
2021-12-20 15:46:35 -0300

    prueba push

--- app/src/main/java/com/example/parkingalkemy/MainActivity.kt
+++ app/src/main/java/com/example/parkingalkemy/MainActivity.kt
@@ -2,10 +2,19 @@ package com.example.parkingalkemy
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import java.util.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+
+        val auto = Vehicle("BGF232" , VehicleType.CAR, Calendar.getInstance())
+        
+
+
+        //val parking= Parking
+
     }
 }
