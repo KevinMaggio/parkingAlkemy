5444eb3d8be8f3f51f02ee9c89d24c43f66eaeac
KevinMaggio <kevin.maggio@mercadolibre.com>
2021-12-20 17:18:45 -0300

    inciando con la carga de vehiculos

--- app/src/main/java/com/example/parkingalkemy/Parking.kt
+++ app/src/main/java/com/example/parkingalkemy/Parking.kt
@@ -2,4 +2,18 @@ package com.example.parkingalkemy
 
 class Parking (
     val vehiclesList:MutableSet<Vehicle>
-)
+){
+
+    val limit =19
+
+    fun addVehicle(vehicle: Vehicle):Boolean{
+        return if (vehiclesList.size<=limit){
+            vehiclesList.add(vehicle)
+            println("Welcome to Alkeparking")
+            true
+        }else {
+            println("Sorry check-infailed")
+            false
+        }
+    }
+}
